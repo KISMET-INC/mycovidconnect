2fd788b6c3c954fecb84c2c9412755212440d5e4
kmoreland909@gmail.com <kmoreland909@gmail.com>
2023-10-28 19:18:10 -0700

    hound-bot-esversion-v1

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   useEffect(() => {
     // import tawk script
 
-    /* jshint esnext: true */
+    /* jshint esversion: 6 */
     ((/** @type {Object} */ Tawk_API) => {
       Tawk_API = Tawk_API || {};
       // const Tawk_LoadStart = new Date();
